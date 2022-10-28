7785e7b2b9d66c7c5c885ae0f786df7facc0c408
jagottsicher <jagottsicher@gmx.net>
2022-10-28 16:42:48 +0800

    updated Code

--- 001-massive-numbers.c
+++ 001-massive-numbers.c
@@ -2,32 +2,31 @@
 
 int main()
 {
-	mpz_t a,b,x;
+    mpz_t a,b,x;
 
-	/* initialize */
-	mpz_init(a);
-	mpz_init(b);
-	mpz_init(x);
+    /* initialize */
+    mpz_init(a);
+    mpz_init(b);
+    mpz_init(x);
 
-	/* set values */
-	mpz_set_ui(a,3000000000);	/* three billion */
-	mpz_set_ui(b,5000000000);	/* five billion */
+    /* set values */
+    mpz_set_ui(a,3000000000);    /* three billion */
+    mpz_set_ui(b,5000000000);    /* five billion */
 
-	/* add the values */
-	mpz_add(x,a,b);
-	/* output */
-	gmp_printf("%Zd + %Zd = %Zd\n",a,b,x);
+    /* add the values */
+    mpz_add(x,a,b);
+    /* output */
+    gmp_printf("%Zd + %Zd = %Zd\n",a,b,x);
 
-	/* multiply */
-//	mpz_init(x);
-	mpz_addmul(x,a,b);
-	/* output */
-	gmp_printf("%Zd * %Zd = %Zd\n",a,b,x);
+    /* multiply */
+    mpz_init(x);
+    mpz_addmul(x,a,b);
+    /* output */
+    gmp_printf("%Zd * %Zd = %Zd\n",a,b,x);
 
-	return(0);
+    return(0);
 }
 
-// Simple test how gmp works:
 //#include <gmp.h>
 //
 //int main()
@@ -42,7 +41,6 @@ int main()
 //    return(0);
 //}
 
-// check if compiler accepts to include gmp header file
 //#include <stdio.h>
 //#include <gmp.h>
 //
@@ -52,21 +50,21 @@ int main()
 //    return(0);
 //}
 
-// Print out a long long
 //#include <stdio.h>
 //
 //int main()
 //{
-//	unsigned long long i = 0;
+//    unsigned long long i = 0;
 //
-//	while(1) {
-//		printf("%llu\n",i);
-//		i++;
-//	}
-//	return(0);
+//    while(1) {
+//        printf("%llu\n",i);
+//    	i++;
+//    }
+//    return(0);
 //}
 
-// iterate over out a long long only
+// 18,446,744,073,709,551,614.
+
 //#include <stdio.h>
 //
 //int main()
